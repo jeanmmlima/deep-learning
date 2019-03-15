90df7c0aeb9914673da22b78342c74e6f742709e
jeanmmlima <jeanmariomlima@gmail.com>
2019-03-15 11:43:22 -0300

    cross validation ok

--- cnn/mnist/mnist_valid_cruzada.py
+++ cnn/mnist/mnist_valid_cruzada.py
@@ -12,3 +12,41 @@ from keras.utils import np_utils
 from keras.layers import Conv2D, MaxPooling2D
 import numpy as np
 from sklearn.model_selection import StratifiedKFold
+
+seed = 5
+np.random.seed(seed)
+
+(X,y),(X_trein, y_trein) = mnist.load_data()
+
+previsores = X.reshape(X.shape[0],28,28,1)
+previsores = previsores.astype('float32')
+
+previsores /= 255
+
+classe = np_utils.to_categorical(y,10)
+
+#controla validação cruzada
+kfold = StratifiedKFold(n_splits = 5, shuffle=True, random_state=seed)
+results = []
+
+b = np.zeros(shape=(classe.shape[0], 1))
+
+for indice_treinamento, indice_teste in kfold.split(previsores, 
+                                                    np.zeros(shape=(classe.shape[0], 1))):
+    #print('Indices treinamento: ',indice_treinamento, 'Indice teste', indice_teste)
+    classificador = Sequential()
+    classificador.add(Conv2D(32,(3,3),input_shape=(28,28,1),activation='relu'))
+    classificador.add(MaxPooling2D(pool_size=(2,2)))
+    classificador.add(Flatten())
+    classificador.add(Dense(units=128,activation='relu'))
+    classificador.add(Dense(units=10,activation='softmax'))
+    classificador.compile(loss='categorical_crossentropy',optimizer='adam',
+                          metrics=['accuracy'])
+    classificador.fit(previsores[indice_treinamento],classe[indice_treinamento],
+                      batch_size=128,epochs=5)
+    precisao = classificador.evaluate(previsores[indice_teste],classe[indice_teste])
+    results.append(precisao[1])
+    
+
+#media = results.mean()
+media = sum(results)/len(results)
