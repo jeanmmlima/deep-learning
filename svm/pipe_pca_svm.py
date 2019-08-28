2de8039edc7f621d0fcfbcf09e2ead7d2497fb53
jeanmmlima <jeanmariomlima@gmail.com>
2019-08-28 17:45:15 -0300

    fresador e non-linear function

--- svm/pipe_pca_svm.py
+++ svm/pipe_pca_svm.py
@@ -13,9 +13,16 @@ from sklearn.model_selection import train_test_split
 from sklearn import metrics
 import matplotlib.pyplot as plt
 
+#### FRESADORA
+
+def fresadora(x):
+    return (2/5) + ((1/10) * np.exp(-5*x)) - ((1/2)*np.exp(-x))
+
+#####
+
 #load data
-X = np.arange(-32,32,0.05,dtype='float32')
-Y = X + X
+X = np.arange(-30,30,0.05,dtype='float32')
+Y = np.sin(X) + 2 * np.cos(X) 
 #Y = np.power(X,2)
 X = X.reshape(-1,1)
 X_train, X_test, y_train, y_test = train_test_split(X,Y,test_size=0.2,random_state=0)
@@ -32,18 +39,27 @@ svr_pca.fit(X_train,y_train)
 y_pred_SVRPCA = svr_pca.predict(X_test)
 y_pred = svr.predict(X_test)
 
-metrics.mean_squared_error(y_pred_SVRPCA,y_test)
-metrics.mean_squared_error(y_pred,y_test)
+print("MSE e MAE: ")
+print(metrics.mean_squared_error(y_test,y_pred_SVRPCA))
+print(metrics.mean_absolute_error(y_test,y_pred_SVRPCA))
+print(metrics.mean_squared_error(y_test,y_pred))
+print(metrics.mean_absolute_error(y_test,y_pred))
+
+print("Variance score: ")
+print(metrics.explained_variance_score(y_test,y_pred_SVRPCA))
+print(metrics.explained_variance_score(y_test,y_pred))
 
 #PLOTTING
 svrs = [svr_pca, svr]
 kernel_label = ['SVR with PCA', 'SVR']
 model_color = ['m','c']
 
-fig
-plt.plot(y_pred_SVRPCA,color='blue',label='ERRO')
-plt.plot(y_pred,color='red')
-fig.text(0.5, 0.04, 'data', ha='center', va='center')
-fig.text(0.06, 0.5, 'target', ha='center', va='center', rotation='vertical')
-fig.suptitle("Support Vector Regression", fontsize=14)
+fig, axs = plt.subplots(nrows = 2, ncols = 2)
+axs[0,0].plot(y_test,color='g')
+axs[0,0].plot(y_pred_SVRPCA,'b:')
+axs[0,1].plot(y_pred_SVRPCA-y_test,color='red')
+axs[0,1].set_xlabel("erro")
+axs[1,0].plot(y_test,color='g')
+axs[1,0].plot(y_pred,'b:')
+axs[1,1].plot(y_pred-y_test,color='red')
 plt.show()
