404118f4ceca25300c3d8c9cebbb24b475a87f10
jeanmmlima <jeanmariomlima@gmail.com>
2019-08-30 14:20:37 -0300

    first attempt dbn

--- rbm/rbm_svm.py
+++ rbm/rbm_svm.py
@@ -31,33 +31,50 @@ Y = normalizer.fit_transform(Y)
 X_train, X_test, y_train, y_test = train_test_split(X,Y,test_size=0.2,random_state=0)
 
 rbm = BernoulliRBM(n_components=1, n_iter=400, learning_rate=0.35, verbose=True, batch_size=30)
+rbm2 = BernoulliRBM(n_components=1, n_iter=200, learning_rate=0.4,verbose=True, batch_size=20)
 svr = SVR(kernel='rbf',C=10000,gamma=0.3,epsilon=0.15)
 rbm_svr = clone(svr)
+rbm2_svr = clone(svr)
 pipe_rbm_svr = Pipeline([(('rbm'),clone(rbm)),(('svr'),clone(svr))])
+
+
+pipe_dbn = Pipeline([(('rbm1'),clone(rbm)),(('rbm2'),clone(rbm)),(('svr'),clone(svr))])
 
 
 rbm.fit(X_train)
 nX = rbm.transform(X_train)
 nX_test = rbm.transform(X_test)
-
-
 rbm_svr.fit(nX,y_train)
+
+rbm2.fit(nX)
+n2X = rbm2.transform(nX)
+n2X_test = rbm2.transform(nX_test)
+rbm2_svr.fit(n2X,y_train)
+
 svr.fit(X_train,y_train)
 pipe_rbm_svr.fit(X_train,y_train)
+pipe_dbn.fit(X_train,y_train)
 
 y_pred_rbmsvr = rbm_svr.predict(nX_test)
 y_pred = svr.predict(X_test)
 y_pred_pipe = pipe_rbm_svr.predict(X_test)
+y_pred_dbn = pipe_dbn.predict(X_test)
+y_pred_rbm2 = pipe_dbn.predict(n2X_test)
 
-print("MSE e MAE: ")
+print("MSE: ")
 print(metrics.mean_squared_error(y_test,y_pred_rbmsvr))
 print(metrics.mean_squared_error(y_test,y_pred))
 print(metrics.mean_squared_error(y_test,y_pred_pipe))
+print(metrics.mean_squared_error(y_test,y_pred_dbn))
+print(metrics.mean_squared_error(y_test,y_pred_rbm2))
 
 print("Variance score: ")
 print(metrics.explained_variance_score(y_test,y_pred_rbmsvr))
 print(metrics.explained_variance_score(y_test,y_pred))
 print(metrics.explained_variance_score(y_test,y_pred_pipe))
+print(metrics.explained_variance_score(y_test,y_pred_dbn))
+print(metrics.explained_variance_score(y_test,y_pred_rbm2))
+
 
 
 
