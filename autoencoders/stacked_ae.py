33b6a71ed61e2a5c67cd0cd933f964ca896a18e5
jeanmmlima <jeanmariomlima@gmail.com>
2019-09-03 13:37:19 -0300

    stacked ae for mnist

--- autoencoders/stacked_ae.py
+++ autoencoders/stacked_ae.py
@@ -11,6 +11,36 @@ import tensorflow as tf
 from tensorflow.contrib.layers import fully_connected
 import numpy as np
 import matplotlib.pyplot as plt
+from tensorflow.examples.tutorials.mnist import input_data
+from functools import partial
+import sys
+import os
+
+
+def plot_image(image, shape=[28, 28]):
+    plt.imshow(image.reshape(shape), cmap="Greys", interpolation="nearest")
+    plt.axis("off")
+
+CHAPTER_ID = "autoencoders"
+
+def save_fig(fig_id, tight_layout=True):
+    path = os.path.join(fig_id + ".png")
+    print("Saving figure", fig_id)
+    if tight_layout:
+        plt.tight_layout()
+    plt.savefig(path, format='png', dpi=300)
+    
+
+
+
+#DATA - MNIST
+mnist = input_data.read_data_sets("/tmp/data/")
+#X, y = mnist["data"], mnist["target"]
+#X_train, X_test, y_train, y_test = X[:60000], X[60000:], y[:60000],y[60000:]
+
+#shuffle_index = np.random.permutation(60000)
+#X_train, y_train = X_train[shuffle_index], y_train[shuffle_index]
+
 
 n_inputs = 28 * 28
 n_hidden1 = 300
@@ -43,4 +73,42 @@ optimizer = tf.train.AdamOptimizer(learning_rate)
 training_op = optimizer.minimize(loss)
 
 init = tf.global_variables_initializer()
+saver = tf.train.Saver() # not shown in the book
+
+n_epochs = 5
+batch_size = 150
+
+#TRANING
+
+with tf.Session() as sess:
+    init.run()
+    for epoch in range(n_epochs):
+        n_batches = mnist.train.num_examples // batch_size
+        for iteration in range (n_batches):
+            print("\r{}%".format(100 * iteration // n_batches), end="") # not shown in the book
+            sys.stdout.flush() 
+            X_batch, y_batch = mnist.train.next_batch(batch_size)
+            sess.run(training_op, feed_dict={X: X_batch})
+        loss_train = reconstruction_loss.eval(feed_dict={X: X_batch})   # not shown
+        print("\r{}".format(epoch), "Train MSE:", loss_train)           # not shown
+        saver.save(sess, "./my_model_all_layers.ckpt") 
+        
+
+def show_reconstructed_digits(X, outputs, model_path = None, n_test_digits = 2):
+    with tf.Session() as sess:
+        if model_path:
+            saver.restore(sess, model_path)
+        X_test = mnist.test.images[:n_test_digits]
+        outputs_val = outputs.eval(feed_dict={X: X_test})
+
+    fig = plt.figure(figsize=(8, 3 * n_test_digits))
+    for digit_index in range(n_test_digits):
+        plt.subplot(n_test_digits, 2, digit_index * 2 + 1)
+        plot_image(X_test[digit_index])
+        plt.subplot(n_test_digits, 2, digit_index * 2 + 2)
+        plot_image(outputs_val[digit_index])
+        
+        
+show_reconstructed_digits(X, outputs, "./my_model_all_layers.ckpt")
+save_fig("reconstruction_plot")
 
