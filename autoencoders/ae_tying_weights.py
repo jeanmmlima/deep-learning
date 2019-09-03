0a22d73559ae08c3c0b78aa00c5ba87a1d8327a8
jeanmmlima <jeanmariomlima@gmail.com>
2019-09-03 17:06:33 -0300

    tied weights tested

--- autoencoders/ae_tying_weights.py
+++ autoencoders/ae_tying_weights.py
@@ -17,6 +17,9 @@ import os
 #ted() function is a bit cumbersome; it’s actually easier to just define the layers manually.
 #The code ends up significantly more verbose:
 
+#data
+mnist = input_data.read_data_sets("/tmp/data/")
+
 n_inputs = 28 * 28
 n_hidden1 = 300
 n_hidden2 = 150  # codings
@@ -42,6 +45,79 @@ weights3 = tf.transpose(weights2, name="weights3") #tied weights
 weights4 = tf.transpose(weights1, name="weights4") #tied weights
 
 #bieases
+biases1 = tf.Variable(tf.zeros(n_hidden1), name= "biases1")
+biases2 = tf.Variable(tf.zeros(n_hidden2), name= "biases2")
+biases3 = tf.Variable(tf.zeros(n_hidden3), name= "biases3")
+biases4 = tf.Variable(tf.zeros(n_outputs), name= "biases4")
+
+#layers
+hidden1 = activation(tf.matmul(X,weights1) + biases1)
+hidden2 = activation(tf.matmul(hidden1,weights2) + biases2)
+hidden3 = activation(tf.matmul(hidden2,weights3) + biases3)
+outputs = tf.matmul(hidden3, weights4) + biases4
+
+reconstruction_loss = tf.reduce_mean(tf.square(outputs - X))
+reg_loss = regularizer(weights1) + regularizer(weights2)
+loss = reconstruction_loss + reg_loss
+
+optimizer = tf.train.AdamOptimizer(learning_rate)
+training_op = optimizer.minimize(loss)
+
+init = tf.global_variables_initializer()
+saver = tf.train.Saver() # not shown in the book
+
+n_epochs = 5
+batch_size = 150
+
+
+with tf.Session() as sess:
+    init.run()
+    for epoch in range(n_epochs):
+        n_batches = mnist.train.num_examples // batch_size
+        for iteration in range (n_batches):
+            print("\r{}%".format(100 * iteration // n_batches), end="") # not shown in the book
+            sys.stdout.flush() 
+            X_batch, y_batch = mnist.train.next_batch(batch_size)
+            sess.run(training_op, feed_dict={X: X_batch})
+        loss_train = reconstruction_loss.eval(feed_dict={X: X_batch})   # not shown
+        print("\r{}".format(epoch), "Train MSE:", loss_train)           # not shown
+        saver.save(sess, "./my_model_all_layers.ckpt") 
+        
+        
+def plot_image(image, shape=[28, 28]):
+    plt.imshow(image.reshape(shape), cmap="Greys", interpolation="nearest")
+    plt.axis("off")
+
+CHAPTER_ID = "autoencoders"
+
+def save_fig(fig_id, tight_layout=True):
+    path = os.path.join(fig_id + ".png")
+    print("Saving figure", fig_id)
+    if tight_layout:
+        plt.tight_layout()
+    plt.savefig(path, format='png', dpi=300)        
+        
+def show_reconstructed_digits(X, outputs, model_path = None, n_test_digits = 2):
+    with tf.Session() as sess:
+        if model_path:
+            saver.restore(sess, model_path)
+        X_test = mnist.test.images[:n_test_digits]
+        outputs_val = outputs.eval(feed_dict={X: X_test})
+
+    fig = plt.figure(figsize=(8, 3 * n_test_digits))
+    for digit_index in range(n_test_digits):
+        plt.subplot(n_test_digits, 2, digit_index * 2 + 1)
+        plot_image(X_test[digit_index])
+        plt.subplot(n_test_digits, 2, digit_index * 2 + 2)
+        plot_image(outputs_val[digit_index])
+        
+        
+show_reconstructed_digits(X, outputs, "./my_model_all_layers.ckpt")
+save_fig("reconstruction_plot")
+        
+
+
+
 
 
 
