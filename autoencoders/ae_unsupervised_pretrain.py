4bb3daf455cb18a463aa87d984af97a8de13f72a
jeanmmlima <jeanmariomlima@gmail.com>
2019-09-03 19:46:27 -0300

    unsupervised pretraining ae pretrain saver

--- autoencoders/ae_unsupervised_pretrain.py
+++ autoencoders/ae_unsupervised_pretrain.py
@@ -19,7 +19,6 @@ mnist = input_data.read_data_sets("/tmp/data/")
 n_inputs = 28 * 28
 n_hidden1 = 300
 n_hidden2 = 150  # codings
-n_hidden3 = n_hidden1
 n_outputs = 10
 
 learning_rate = 0.01
@@ -30,24 +29,38 @@ regularizer = tf.contrib.layers.l2_regularizer(l2_reg)
 initializer = tf.contrib.layers.variance_scaling_initializer()
 
 X = tf.placeholder(tf.float32, shape=[None, n_inputs])
+y = tf.placeholder(tf.int32, shape=[None])
 
 weights1_init = initializer([n_inputs,n_hidden1])
 weights2_init = initializer([n_hidden1, n_hidden2])
+weights3_init = initializer([n_hidden2, n_outputs])
 
 #weights
 weights1 = tf.Variable(weights1_init, dtype=tf.float32, name="weights1")
 weights2 = tf.Variable(weights2_init, dtype=tf.float32, name="weights2")
-weights3 = tf.transpose(weights2, name="weights3") #tied weights
-weights4 = tf.transpose(weights1, name="weights4") #tied weights
+weights3 = tf.Variable(weights3_init, dtype=tf.float32, name="weights3")
 
 #bieases
 biases1 = tf.Variable(tf.zeros(n_hidden1), name= "biases1")
 biases2 = tf.Variable(tf.zeros(n_hidden2), name= "biases2")
-biases3 = tf.Variable(tf.zeros(n_hidden3), name= "biases3")
-biases4 = tf.Variable(tf.zeros(n_outputs), name= "biases4")
+biases3 = tf.Variable(tf.zeros(n_outputs), name= "biases3")
 
 #layers
 hidden1 = activation(tf.matmul(X,weights1) + biases1)
 hidden2 = activation(tf.matmul(hidden1,weights2) + biases2)
-hidden3 = activation(tf.matmul(hidden2,weights3) + biases3)
-outputs = tf.matmul(hidden3, weights4) + biases4
+logits = tf.matmul(hidden2, weights3) + biases3
+
+cross_entropy = tf.nn.sparse_softmax_cross_entropy_with_logits(labels=y,logits=logits)
+reg_loss = regularizer(weights1) + regularizer(weights2) + regularizer(weights3)
+loss = cross_entropy + reg_loss
+optimizer = tf.train.AdamOptimizer(learning_rate)
+training_op = optimizer.minimize(loss)
+
+correct = tf.nn.in_top_k(logits, y, 1)
+accuracy = tf.reduce_mean(tf.cast(correct, tf.float32))
+
+init = tf.global_variables_initializer()
+pretrain_saver = tf.train.Saver([weights1, weights2, biases1, biases2])
+saver = tf.train.Saver() # not shown in the book
+
+
