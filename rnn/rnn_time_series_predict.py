4503c8d664733799bb616ec4baf6399ac03731cc
jeanmmlima <jeanmariomlima@gmail.com>
2019-09-16 18:31:29 -0300

    with OutpurProjectionWrapper and without it

--- rnn/rnn_time_series_predict.py
+++ rnn/rnn_time_series_predict.py
@@ -78,7 +78,7 @@ save_fig("time_series_plot")
 plt.show()
 
 
-### IMPLEMENTATIONS
+### IMPLEMENTATIONS #####
 
 reset_graph()
 
@@ -147,6 +147,75 @@ plt.xlabel("Time")
 
 save_fig("time_series_pred_plot")
 plt.show()
+
+#Although using an OutputProjectionWrapper is the simplest solution to reduce the
+#dimensionality of the RNN’s output sequences down to just one value per time step
+#(per instance), it is not the most efficient. There is a trickier but more efficient solution:
+#you can reshape the RNN outputs from [batch_size, n_steps, n_neurons]
+#to [batch_size * n_steps, n_neurons], then apply a single fully connected layer
+#with the appropriate output size (in our case just 1), which will result in an output
+#tensor of shape [batch_size * n_steps, n_outputs], and then reshape this tensor
+#to [batch_size, n_steps, n_outputs].
+
+reset_graph()
+
+n_steps = 20
+n_inputs = 1
+n_neurons = 100
+n_outputs = 1
+learning_rate = 0.001
+
+X = tf.placeholder(tf.float32, [None, n_steps, n_inputs])
+y = tf.placeholder(tf.float32, [None,n_steps, n_outputs])
+cell = tf.contrib.rnn.BasicRNNCell(num_units=n_neurons, activation=tf.nn.relu)
+rnn_outputs, states = tf.nn.dynamic_rnn(cell, X, dtype=tf.float32)
+
+#Then we stack all the outputs using the reshape() operation, apply the fully connected
+#linear layer (without using any activation function; this is just a projection), and
+#finally unstack all the outputs, again using reshape():
+
+## WITHOUT USING AN OUTPUT PROJECTION WRAPPER
+
+stacked_rnn_outputs = tf.reshape(rnn_outputs, [-1, n_neurons])
+stacked_outputs = tf.layers.dense(stacked_rnn_outputs, n_outputs)
+outputs = tf.reshape(stacked_outputs, [-1,n_steps,n_outputs])
+
+loss = tf.reduce_mean(tf.square(outputs - y))
+optimizer = tf.train.AdamOptimizer(learning_rate=learning_rate)
+training_op = optimizer.minimize(loss)
+
+init = tf.global_variables_initializer()
+
+saver = tf.train.Saver()
+
+n_iterations = 1500
+batch_size = 50
+
+with tf.Session() as sess:
+    init.run()
+    for iteration in range(n_iterations):
+        X_batch, y_batch = next_batch(batch_size,n_steps)
+        sess.run(training_op, feed_dict={X: X_batch, y: y_batch})
+        if iteration % 100 == 0:
+            mse = loss.eval(feed_dict={X: X_batch, y: y_batch})
+            print(iteration, "\tMSE:", mse)
+            
+    X_new = time_series(np.array(t_instance[:-1].reshape(-1, n_steps, n_inputs)))
+    y_pred = sess.run(outputs, feed_dict={X: X_new})
+    
+    saver.save(sess, "my_time_series_model")
+    
+print(y_pred)
+    
+plt.title("Testing the model", fontsize=14)
+plt.plot(t_instance[:-1], time_series(t_instance[:-1]), "bo", markersize=10, label="instance")
+plt.plot(t_instance[1:], time_series(t_instance[1:]), "w*", markersize=10, label="target")
+plt.plot(t_instance[1:], y_pred[0,:,0], "r.", markersize=10, label="prediction")
+plt.legend(loc="upper left")
+plt.xlabel("Time")
+
+plt.show()
+
 
 
 
