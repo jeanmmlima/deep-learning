f17682686969adaac4ef378dd5b6c024c17aa729
jeanmmlima <jeanmariomlima@gmail.com>
2019-09-16 17:36:39 -0300

    rnn for time series prediction - complete

--- rnn/rnn_time_series_predict.py
+++ rnn/rnn_time_series_predict.py
@@ -30,7 +30,7 @@ PROJECT_ROOT_DIR = "."
 CHAPTER_ID = "rnn"
 
 def save_fig(fig_id, tight_layout=True):
-    path = os.path.join(PROJECT_ROOT_DIR, "images", CHAPTER_ID, fig_id + ".png")
+    path = os.path.join(fig_id + ".png")
     print("Saving figure", fig_id)
     if tight_layout:
         plt.tight_layout()
@@ -46,3 +46,107 @@ def time_series(t):
 
 def next_batch(batch_size, n_steps):
     t0 = np.random.rand(batch_size, 1) * (t_max - t_min - n_steps * resolution)
+    Ts = t0 + np.arange(0., n_steps + 1) * resolution
+    ys = time_series(Ts)
+    return ys[:, :-1].reshape(-1, n_steps, 1), ys[:, 1:].reshape(-1, n_steps, 1)
+
+
+t = np.linspace(t_min, t_max, int((t_max - t_min) / resolution))
+
+n_steps = 20
+t_instance = np.linspace(12.2, 12.2 + resolution * (n_steps + 1), n_steps + 1)
+
+plt.figure(figsize=(11,4))
+plt.subplot(121)
+plt.title("A time series (generated)", fontsize=14)
+plt.plot(t, time_series(t), label=r"$t . \sin(t) / 3 + 2 . \sin(5t)$")
+plt.plot(t_instance[:-1], time_series(t_instance[:-1]), "b-", linewidth=3, label="A training instance")
+plt.legend(loc="lower left", fontsize=14)
+plt.axis([0, 30, -17, 13])
+plt.xlabel("Time")
+plt.ylabel("Value")
+
+plt.subplot(122)
+plt.title("A training instance", fontsize=14)
+plt.plot(t_instance[:-1], time_series(t_instance[:-1]), "bo", markersize=10, label="instance")
+plt.plot(t_instance[1:], time_series(t_instance[1:]), "w*", markersize=10, label="target")
+plt.legend(loc="upper left")
+plt.xlabel("Time")
+
+
+save_fig("time_series_plot")
+plt.show()
+
+
+### IMPLEMENTATIONS
+
+reset_graph()
+
+n_steps = 20
+n_inputs = 1
+n_neurons = 100
+n_outputs = 1
+
+X = tf.placeholder(tf.float32, [None, n_steps, n_inputs])
+y = tf.placeholder(tf.float32, [None,n_steps, n_outputs])
+cell = tf.contrib.rnn.BasicRNNCell(num_units=n_neurons, activation=tf.nn.relu)
+outputs, states = tf.nn.dynamic_rnn(cell, X, dtype=tf.float32)
+
+#At each time step we now have an output vector of size 100. But what we actually
+#want is a single output value at each time step. The simplest solution is to wrap the
+#cell in an OutputProjectionWrapper. A cell wrapper acts like a normal cell, proxying
+#every method call to an underlying cell, but it also adds some functionality. The Out
+#putProjectionWrapper adds a fully connected layer of linear neurons (i.e., without
+#any activation function) on top of each output (but it does not affect the cell state).
+#All these fully connected layers share the same (trainable) weights and bias terms.
+
+cell = tf.contrib.rnn.OutputProjectionWrapper(
+        tf.contrib.rnn.BasicRNNCell(num_units=n_neurons, activation=tf.nn.relu),
+        output_size=n_outputs)
+outputs, states = tf.nn.dynamic_rnn(cell,X,dtype=tf.float32)
+
+#define the cost function: MSE
+
+learning_rate = 0.001
+loss = tf.reduce_mean(tf.square(outputs - y))
+optimizer = tf.train.AdamOptimizer(learning_rate=learning_rate)
+training_op = optimizer.minimize(loss)
+
+init = tf.global_variables_initializer()
+
+saver = tf.train.Saver()
+
+n_iterations = 10000
+batch_size = 50
+
+with tf.Session() as sess:
+    init.run()
+    for iteration in range(n_iterations):
+        X_batch, y_batch = next_batch(batch_size,n_steps)
+        sess.run(training_op, feed_dict={X: X_batch, y: y_batch})
+        if iteration % 100 == 0:
+            mse = loss.eval(feed_dict={X: X_batch, y: y_batch})
+            print(iteration, "MSE:", mse)
+            
+    saver.save(sess, "my_time_series_model")
+
+
+
+with tf.Session() as sess:                          # not shown in the book
+    saver.restore(sess, "my_time_series_model")   # not shown
+
+    X_new = time_series(np.array(t_instance[:-1].reshape(-1, n_steps, n_inputs)))
+    y_pred = sess.run(outputs, feed_dict={X: X_new})
+
+plt.title("Testing the model", fontsize=14)
+plt.plot(t_instance[:-1], time_series(t_instance[:-1]), "bo", markersize=10, label="instance")
+plt.plot(t_instance[1:], time_series(t_instance[1:]), "w*", markersize=10, label="target")
+plt.plot(t_instance[1:], y_pred[0,:,0], "r.", markersize=10, label="prediction")
+plt.legend(loc="upper left")
+plt.xlabel("Time")
+
+save_fig("time_series_pred_plot")
+plt.show()
+
+
+
