071ee83163155c799a577e021b36470bf66aee8e
jeanmmlima <jeanmariomlima@gmail.com>
2019-09-17 17:37:00 -0300

    deep rnn with dropout layer

--- rnn/deep_rnn.py
+++ rnn/deep_rnn.py
@@ -104,10 +104,83 @@ with tf.Session() as sess:
 outputs_val.shape
 
 
-### APPLYING DRPOUT
+### APPLYING DROPOUT
 
 #If you build a very deep RNN, it may end up overfitting the training set. To prevent
 #that, a common technique is to apply dropout (introduced in Chapter 11). You can
 #simply add a dropout layer before or after the RNN as usual, but if you also want to
 #apply dropout between the RNN layers, you need to use a DropoutWrapper.
+
+reset_graph()
+
+n_inputs = 1
+n_neurons = 100
+n_layers = 3
+n_steps = 20
+n_outputs = 1
+
+X = tf.placeholder(tf.float32, [None, n_steps, n_inputs])
+y = tf.placeholder(tf.float32, [None, n_steps, n_outputs])
+
+#Note: the input_keep_prob parameter can be a placeholder, 
+#making it possible to set it to any value you want during training, 
+#and to 1.0 during testing (effectively turning dropout off). 
+#This is a much more elegant solution than what was recommended 
+#in earlier versions of the book (i.e., writing your own wrapper class or
+#having a separate model for training and testing). 
+
+keep_prob = tf.placeholder_with_default(1.0, shape=())
+
+cells = [tf.nn.rnn_cell.BasicRNNCell(num_units=n_neurons)
+         for layer in range(n_layers)]
+cells_drop = [tf.nn.rnn_cell.DropoutWrapper(cell, input_keep_prob=keep_prob)
+              for cell in cells]
+multi_layer_cell = tf.nn.rnn_cell.MultiRNNCell(cells_drop)
+rnn_outputs, states = tf.nn.dynamic_rnn(multi_layer_cell, X, dtype=tf.float32)
+
+learning_rate = 0.01
+
+stacked_rnn_outputs = tf.reshape(rnn_outputs, [-1, n_neurons])
+stacked_outputs = tf.layers.dense(stacked_rnn_outputs, n_outputs)
+outputs = tf.reshape(stacked_outputs, [-1, n_steps, n_outputs])
+
+loss = tf.reduce_mean(tf.square(outputs - y))
+optimizer = tf.train.AdamOptimizer(learning_rate=learning_rate)
+training_op = optimizer.minimize(loss)
+
+init = tf.global_variables_initializer()
+saver = tf.train.Saver()
+
+n_iterations = 1500
+batch_size = 50
+train_keep_prob = 0.5
+
+with tf.Session() as sess:
+    init.run()
+    for iteration in range(n_iterations):
+        X_batch, y_batch = next_batch(batch_size, n_steps)
+        _, mse = sess.run([training_op, loss],
+                          feed_dict={X: X_batch, y: y_batch,
+                                     keep_prob: train_keep_prob})
+        if iteration % 100 == 0:                   # not shown in the book
+            print(iteration, "Training MSE:", mse) # not shown
+    
+    saver.save(sess, "./my_dropout_time_series_model")
+    
+
+with tf.Session() as sess:
+    saver.restore(sess, "./my_dropout_time_series_model")
+
+    X_new = time_series(np.array(t_instance[:-1].reshape(-1, n_steps, n_inputs)))
+    y_pred = sess.run(outputs, feed_dict={X: X_new})
+
+
+plt.title("Testing the model", fontsize=14)
+plt.plot(t_instance[:-1], time_series(t_instance[:-1]), "bo", markersize=10, label="instance")
+plt.plot(t_instance[1:], time_series(t_instance[1:]), "w*", markersize=10, label="target")
+plt.plot(t_instance[1:], y_pred[0,:,0], "r.", markersize=10, label="prediction")
+plt.legend(loc="upper left")
+plt.xlabel("Time")
+
+plt.show()                            
 
