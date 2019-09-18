94b63eeb04729951d49b4d438a3301b36a21feea
jeanmmlima <jeanmariomlima@gmail.com>
2019-09-18 18:48:37 -0300

    lstm tested

--- rnn/tf_lstm.py
+++ rnn/tf_lstm.py
@@ -5,3 +5,137 @@ Created on Tue Sep 17 17:43:53 2019
 @author: Jean Mário
 """
 
+from __future__ import division, print_function, unicode_literals
+
+# Common imports
+import numpy as np
+import os
+
+# to make this notebook's output stable across runs
+def reset_graph(seed=42):
+    tf.reset_default_graph()
+    tf.set_random_seed(seed)
+    np.random.seed(seed)
+
+# To plot pretty figures
+%matplotlib inline
+import matplotlib
+import matplotlib.pyplot as plt
+plt.rcParams['axes.labelsize'] = 14
+plt.rcParams['xtick.labelsize'] = 12
+plt.rcParams['ytick.labelsize'] = 12
+
+# Where to save the figures
+PROJECT_ROOT_DIR = "."
+CHAPTER_ID = "rnn"
+
+def save_fig(fig_id, tight_layout=True):
+    path = os.path.join(fig_id + ".png")
+    print("Saving figure", fig_id)
+    if tight_layout:
+        plt.tight_layout()
+    plt.savefig(path, format='png', dpi=300)
+    
+import tensorflow as tf
+
+t_min, t_max = 0, 30
+resolution = 0.1
+
+def time_series(t):
+    return t * np.sin(t) / 3 + 2 * np.sin(t*5)
+
+def next_batch(batch_size, n_steps):
+    t0 = np.random.rand(batch_size, 1) * (t_max - t_min - n_steps * resolution)
+    Ts = t0 + np.arange(0., n_steps + 1) * resolution
+    ys = time_series(Ts)
+    return ys[:, :-1].reshape(-1, n_steps, 1), ys[:, 1:].reshape(-1, n_steps, 1)
+
+
+def shuffle_batch(X, y, batch_size):
+    rnd_idx = np.random.permutation(len(X))
+    n_batches = len(X) // batch_size
+    for batch_idx in np.array_split(rnd_idx, n_batches):
+        X_batch, y_batch = X[batch_idx], y[batch_idx]
+        yield X_batch, y_batch
+        
+    (X_train, y_train), (X_test, y_test) = tf.keras.datasets.mnist.load_data()
+X_train = X_train.astype(np.float32).reshape(-1, 28*28) / 255.0
+X_test = X_test.astype(np.float32).reshape(-1, 28*28) / 255.0
+y_train = y_train.astype(np.int32)
+y_test = y_test.astype(np.int32)
+X_valid, X_train = X_train[:5000], X_train[5000:]
+y_valid, y_train = y_train[:5000], y_train[5000:]
+
+
+t = np.linspace(t_min, t_max, int((t_max - t_min) / resolution))
+
+n_steps = 20
+t_instance = np.linspace(12.2, 12.2 + resolution * (n_steps + 1), n_steps + 1)
+
+plt.figure(figsize=(11,4))
+plt.subplot(121)
+plt.title("A time series (generated)", fontsize=14)
+plt.plot(t, time_series(t), label=r"$t . \sin(t) / 3 + 2 . \sin(5t)$")
+plt.plot(t_instance[:-1], time_series(t_instance[:-1]), "b-", linewidth=3, label="A training instance")
+plt.legend(loc="lower left", fontsize=14)
+plt.axis([0, 30, -17, 13])
+plt.xlabel("Time")
+plt.ylabel("Value")
+
+plt.subplot(122)
+plt.title("A training instance", fontsize=14)
+plt.plot(t_instance[:-1], time_series(t_instance[:-1]), "bo", markersize=10, label="instance")
+plt.plot(t_instance[1:], time_series(t_instance[1:]), "w*", markersize=10, label="target")
+plt.legend(loc="upper left")
+plt.xlabel("Time")
+
+
+save_fig("time_series_plot")
+plt.show()
+
+### IMPLEMENTATION DEEP LSTM
+
+reset_graph()
+
+n_steps = 28
+n_inputs = 28
+n_neurons = 150
+n_outputs = 10
+n_layers = 3
+
+learning_rate = 0.001
+
+X = tf.placeholder(tf.float32, [None, n_steps, n_inputs])
+y = tf.placeholder(tf.int32, [None])
+
+lstm_cells = [tf.nn.rnn_cell.BasicLSTMCell(num_units=n_neurons)
+                for layer in range(n_layers)]
+
+multi_cell = tf.nn.rnn_cell.MultiRNNCell(lstm_cells)
+outputs, states = tf.nn.dynamic_rnn(multi_cell, X, dtype=tf.float32)
+
+top_layer_h_state = states[-1][1]
+logits = tf.layers.dense(top_layer_h_state, n_outputs, name="softmax")
+xentropy = tf.nn.sparse_softmax_cross_entropy_with_logits(labels=y, logits=logits)
+
+loss = tf.reduce_mean(xentropy, name="loss")
+optimizer = tf.train.AdamOptimizer(learning_rate=learning_rate)
+training_op = optimizer.minimize(loss)
+correct = tf.nn.in_top_k(logits, y, 1)
+accuracy = tf.reduce_mean(tf.cast(correct, tf.float32))
+    
+init = tf.global_variables_initializer()
+
+n_epochs = 10
+batch_size = 150
+
+with tf.Session() as sess:
+    init.run()
+    for epoch in range(n_epochs):
+        for X_batch, y_batch in shuffle_batch(X_train, y_train, batch_size):
+            X_batch = X_batch.reshape((-1, n_steps, n_inputs))
+            sess.run(training_op, feed_dict={X: X_batch, y: y_batch})
+        acc_batch = accuracy.eval(feed_dict={X: X_batch, y: y_batch})
+        acc_test = accuracy.eval(feed_dict={X: X_test, y: y_test})
+        print(epoch, "Last batch accuracy:", acc_batch, "Test accuracy:", acc_test)
+
