7a770a448d97e0a7a6294a17ca1dfa96b299a58d
jeanmmlima <jeanmariomlima@gmail.com>
2019-09-19 17:01:09 -0300

    saving, restoring and mini-batch gradient descent

--- tensor_flow_basics/tf_basics.py
+++ tensor_flow_basics/tf_basics.py
@@ -183,4 +183,144 @@ with tf.Session() as sess:
 
 print("Best theta:")
 print(best_theta)
+
+#Momentum Optimizer
+
+reset_graph()
+
+n_epochs = 1000
+learning_rate = 0.01
+
+X = tf.constant(scaled_housing_data_plus_bias, dtype=tf.float32, name="X")
+y = tf.constant(housing.target.reshape(-1, 1), dtype=tf.float32, name="y")
+theta = tf.Variable(tf.random_uniform([n + 1, 1], -1.0, 1.0, seed=42), name="theta")
+y_pred = tf.matmul(X, theta, name="predictions")
+error = y_pred - y
+mse = tf.reduce_mean(tf.square(error), name="mse")
+
+optimizer = tf.train.MomentumOptimizer(learning_rate=learning_rate,
+                                       momentum=0.9)
+
+training_op = optimizer.minimize(mse)
+
+init = tf.global_variables_initializer()
+
+with tf.Session() as sess:
+    sess.run(init)
+
+    for epoch in range(n_epochs):
+        sess.run(training_op)
     
+    best_theta = theta.eval()
+
+print("Best theta:")
+print(best_theta)
+
+#Mini-batch Gradient Descent
+
+n_epochs = 1000
+learning_rate = 0.01
+
+reset_graph()
+
+X = tf.placeholder(tf.float32, shape=(None, n + 1), name="X")
+y = tf.placeholder(tf.float32, shape=(None, 1), name="y")
+
+
+theta = tf.Variable(tf.random_uniform([n + 1, 1], -1.0, 1.0, seed=42), name="theta")
+y_pred = tf.matmul(X, theta, name="predictions")
+error = y_pred - y
+mse = tf.reduce_mean(tf.square(error), name="mse")
+optimizer = tf.train.GradientDescentOptimizer(learning_rate=learning_rate)
+training_op = optimizer.minimize(mse)
+
+init = tf.global_variables_initializer()
+
+n_epochs = 10
+
+batch_size = 100
+n_batches = int(np.ceil(m / batch_size))
+
+def fetch_batch(epoch, batch_index, batch_size):
+    np.random.seed(epoch * n_batches + batch_index)  # not shown in the book
+    indices = np.random.randint(m, size=batch_size)  # not shown
+    X_batch = scaled_housing_data_plus_bias[indices] # not shown
+    y_batch = housing.target.reshape(-1, 1)[indices] # not shown
+    return X_batch, y_batch
+
+with tf.Session() as sess:
+    sess.run(init)
+
+    for epoch in range(n_epochs):
+        for batch_index in range(n_batches):
+            X_batch, y_batch = fetch_batch(epoch, batch_index, batch_size)
+            sess.run(training_op, feed_dict={X: X_batch, y: y_batch})
+
+    best_theta = theta.eval()
+    
+## Saving and restoring models
+    
+reset_graph()
+
+n_epochs = 1000                                                                       # not shown in the book
+learning_rate = 0.01                                                                  # not shown
+
+X = tf.constant(scaled_housing_data_plus_bias, dtype=tf.float32, name="X")            # not shown
+y = tf.constant(housing.target.reshape(-1, 1), dtype=tf.float32, name="y")            # not shown
+theta = tf.Variable(tf.random_uniform([n + 1, 1], -1.0, 1.0, seed=42), name="theta")
+y_pred = tf.matmul(X, theta, name="predictions")                                      # not shown
+error = y_pred - y                                                                    # not shown
+mse = tf.reduce_mean(tf.square(error), name="mse")                                    # not shown
+optimizer = tf.train.GradientDescentOptimizer(learning_rate=learning_rate)            # not shown
+training_op = optimizer.minimize(mse)                                                 # not shown
+
+init = tf.global_variables_initializer()
+saver = tf.train.Saver()
+
+with tf.Session() as sess:
+    sess.run(init)
+
+    for epoch in range(n_epochs):
+        if epoch % 100 == 0:
+            print("Epoch", epoch, "MSE =", mse.eval())                                # not shown
+            save_path = saver.save(sess, "/tmp/my_model.ckpt")
+        sess.run(training_op)
+    
+    best_theta = theta.eval()
+    save_path = saver.save(sess, "/tmp/my_model_final.ckpt")
+    
+    
+with tf.Session() as sess:
+    saver.restore(sess, "/tmp/my_model_final.ckpt")
+    best_theta_restored = theta.eval() # not shown in the book
+    
+np.allclose(best_theta, best_theta_restored)
+
+
+#If you want to have a saver that loads and restores theta with 
+#a different name, such as "weights":
+
+saver = tf.train.Saver({"weights": theta})
+
+#By default the saver also saves the graph structure itself in a second 
+#file with the extension .meta. You can use the function tf.train.import_meta_graph() 
+#to restore the graph structure. This function loads the graph into the default graph 
+#and returns a Saver that can then be used to restore 
+#the graph state (i.e., the variable values):
+
+reset_graph()
+# notice that we start with an empty graph.
+
+saver = tf.train.import_meta_graph("/tmp/my_model_final.ckpt.meta")  # this loads the graph structure
+theta = tf.get_default_graph().get_tensor_by_name("theta:0") # not shown in the book
+
+with tf.Session() as sess:
+    saver.restore(sess, "/tmp/my_model_final.ckpt")  # this restores the graph's state
+    best_theta_restored = theta.eval() # not shown in the book
+
+np.allclose(best_theta, best_theta_restored)
+
+#This means that you can import a pretrained model without having to have
+# the corresponding Python code to build the graph. This is very handy when 
+#you keep tweaking and saving your model: you can load a previously saved model
+# without having to search for the version of the code that built it.
