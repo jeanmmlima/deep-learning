686f09b06b18f1891fa0b1f181efbfae9a9a0be3
jeanmmlima <jeanmariomlima@gmail.com>
2019-11-13 16:00:55 -0300

    pandas

--- activate_functions.py
+++ activate_functions.py
@@ -7,6 +7,7 @@ Created on Thu Mar  7 11:03:45 2019
 """
 
 import numpy as np
+import matplotlib.pyplot as plt
 
 def step(sum):
     if (sum >= 1):
@@ -25,9 +26,7 @@ def tanh(sum):
     # Utilizada em redes neurais convolucionais e profundas
     #Tende a ter melhores resultados com essa funcao de ativacao
 def relu(sum):
-    if (sum >= 0):
-        return sum
-    return 0
+    return np.maximum(0,sum)
 
 #Linear Function - Utilizada par exemplos de regressão
 def linear(sum):
@@ -39,4 +38,33 @@ def linear(sum):
 def softmax(x):
     ex = np.exp(x)
     return ex/ex.sum()
+
+
+
+#dados
+x = np.arange(-3,3,0.01)    
+
+y_tanh = tanh(x)
+y_sig = sigmoide(x)
+y_relu= relu(x)
+
+
+fig = plt.figure()
+plt.plot(x,y_tanh,label='tanh',linewidth=2,linestyle='-.')
+plt.plot(x,y_sig,label='sigmoide',linewidth=2, linestyle='--')
+plt.plot(x,y_relu,label='ReLU',linewidth=2)
+plt.legend()
+plt.grid()
+plt.title("Funções de Ativação")
+
+plt.show()
+
+fig.savefig('af.eps')
+fig.savefig('af.png')
+
+
+
+
+
+
 
---
+++ pandas/pandas1.py
@@ -0,0 +1,77 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Nov 13 14:56:42 2019
+
+@author: Jean Mário
+"""
+
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+
+#criando uma serie
+series = pd.Series([1,3,5,np.nan,6,8])
+
+#criando dataframe
+
+dates = pd.date_range('20191101',periods=6)
+
+df = pd.DataFrame(np.random.randn(6,4), index=dates, columns=list('ABCD'))
+
+#describe() shows a quick statistic summary of your data:
+
+df.describe()
+
+#Transposing your data:
+
+df.T
+
+#Selection
+
+#1. getting
+df['A']
+df[0:3]
+df['20191101':'20191103']
+
+#. By POSITION
+ #position
+df.iloc[0]
+df.iloc[0:2]
+ #slices
+df.iloc[3:5,0:2]
+#lista de posicoes
+df.iloc[[1,2,4],[0,2]]
+
+#slicing colunas e linhas de maneira explicita
+df.iloc[1:3,:]
+df.iloc[:,1:3]
+
+#valor explicito
+df.iloc[1,1]
+
+#Boolean indexing
+df[df.A > 0]
+
+
+# Plots
+ts = pd.Series(np.random.randn(1000),index=pd.date_range('1/1/2000', periods=1000))
+ts = ts.cumsum()
+ts.plot()
+
+df = pd.DataFrame(np.random.randn(1000,4), index=ts.index, columns=['A','B','C','D'])
+df = df.cumsum()
+
+plt.figure()
+df.plot()
+plt.legend(loc='best')
+plt.savefig('plotting-with-pandas.png')
+
+# CSV files
+
+df.to_csv("foo.csv")
+
+tanks = pd.read_csv('data_tanks_quanser.csv')
+
+
+
